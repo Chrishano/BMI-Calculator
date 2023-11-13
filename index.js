d454a1e3749bbbfb9bd24f7f15c8461a043c46ab
ChrishanoB <chrishanoburns20@gmail.com>
2023-11-13 15:02:42 +0200

    Edited JS

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var age = document.getElementById("age");
-var height = document.getElementById("height");
-var weight = document.getElementById("weight");
-var male = document.getElementById("m");
-var female = document.getElementById("f");
-var form = document.getElementById("form");
+let age = document.getElementById("age");
+let height = document.getElementById("height");
+let weight = document.getElementById("weight");
+let male = document.getElementById("m");
+let female = document.getElementById("f");
+let form = document.getElementById("form");
 
 function validateForm(){
   if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
@@ -16,16 +16,16 @@ function validateForm(){
 document.getElementById("submit").addEventListener("click", validateForm);
 
 function countBmi(){
-  var p = [age.value, height.value, weight.value];
+  let p = [age.value, height.value, weight.value];
   if(male.checked){
     p.push("male");
   }else if(female.checked){
     p.push("female");
   }
   form.reset();
-  var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+  let bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
       
-  var result = '';
+  let result = '';
   if(bmi<18.5){
     result = 'Underweight';
      }else if(18.5<=bmi&&bmi<=24.9){
@@ -38,12 +38,12 @@ function countBmi(){
     result = 'Extremely obese';
      }
   
-  var h1 = document.createElement("h1");
-  var h2 = document.createElement("h2");
+  let h1 = document.createElement("h1");
+  let h2 = document.createElement("h2");
 
-  var t = document.createTextNode(result);
-  var b = document.createTextNode('BMI: ');
-  var r = document.createTextNode(parseFloat(bmi).toFixed(2));
+  let t = document.createTextNode(result);
+  let b = document.createTextNode('BMI: ');
+  let r = document.createTextNode(parseFloat(bmi).toFixed(2));
   
   h1.appendChild(t);
   h2.appendChild(b);
